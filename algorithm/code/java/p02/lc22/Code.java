6b5da15ac2b3171959ac24c80d8abdf84bbbca88
Chen's <3357541936@qq.com>
2023-12-22 23:54:32 +0800

    fix: lc22/Code.java

--- algorithm/code/java/p02/lc22/Code.java
+++ algorithm/code/java/p02/lc22/Code.java
@@ -13,8 +13,9 @@ public class Code {
 
     /**
      * tips: 当 左侧括号 的剩余数量 超过 右侧括号 的剩余数量 => 即: 右括号使用量 > 左括号使用量, 条件则不成立
-     * @param str String 最终需要放入 ans 的字符串
-     * @param left int 左括号的剩余量, 当剩余量为 0 时, 表示已经用尽
+     *
+     * @param str   String 最终需要放入 ans 的字符串
+     * @param left  int 左括号的剩余量, 当剩余量为 0 时, 表示已经用尽
      * @param right int 有括号的剩余量, 同上...
      */
     public void abs(String str, int left, int right) {
@@ -26,10 +27,23 @@ public class Code {
             return;
         }
         if (left > 0) {
+            // 回溯
+//            str += "(";
+//            abs(str,left-1,right);
+//            str = str.substring(0, str.length() -1);
+
+
             abs(str + "(", left - 1, right);
         }
         if (right > 0) {
+            // 回溯
+//            str += ")";
+//            abs(str,left,right-1);
+//            str = str.substring(0, str.length() -1);
+
             abs(str + ")", left, right - 1);
         }
+
+
     }
 }
